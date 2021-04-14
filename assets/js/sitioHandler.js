ee301278485d22f01b34ab016cb5cd2228715f79
NunoReis84314 <43752773+NunoReis84314@users.noreply.github.com>
2021-04-14 22:03:20 +0100

    oneclick

---
+++ assets/js/sitioHandler.js
@@ -0,0 +1,28 @@
+function fetchSondagens(id_sitio){
+    return fetch('https://ptsibackend.herokuapp.com/sitio/'+id_sitio, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      }).then(result => {
+            response = result.json();
+            return response;
+      }).catch((error) => {return error})
+};
+
+function fetchUEs(id_sitio){
+    return fetch('https://ptsibackend.herokuapp.com/sitioUEIDsitio/'+id_sitio, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      }).then(result => {
+            response = result.json();
+            if (response.length == 0) {
+                let vazio = "";
+                return vazio;
+            } else {
+                return response;
+            }
+      }).catch((error) => {return error})
+}
